6b4709feb4e1386cfd2124ae33254575417b173c
Piyush Asutkar <asutkarpeeyush@gmail.com>
2023-11-03 21:09:35 +0530

    [wc tool] line count and word count implementation added

--- wc_tool/README.md
+++ wc_tool/README.md
@@ -1,5 +1,13 @@
 Problem: https://codingchallenges.fyi/challenges/challenge-wc/
 
+#### Installation
+- go build .
+- ./wc_tool <option> <file_name>
+
+
+#### Potential improvements
+- needs to read entire file to memory
+
 #### Learnings:
 - Golang
     - flag parsing
--- wc_tool/main.go
+++ wc_tool/main.go
@@ -12,17 +12,21 @@ import (
 	"os"
 )
 
-var fileName string
+var bytesCount string
+var lineCount string
+var wordCount string
+
 var usage = `Usage:
 NAME
-	ccwc
+	./wc_tool
 
 DESCRIPTION
 	-c <filepath>: outputs the number of bytes in a file
 `
 
+// fileBytes returns the count of bytes in a file.
 func fileBytes() (int, error) {
-	content, err := os.ReadFile(fileName) // returns a byte array of content
+	content, err := os.ReadFile(bytesCount) // returns a byte array of content
 	if err != nil {
 		return 0, err
 	}
@@ -30,22 +34,87 @@ func fileBytes() (int, error) {
 	return len(content), nil
 }
 
-func main() {
-	// TODO: The file is assumed to be in same directory.
-	flag.StringVar(&fileName, "c", "", "File name to be parsed")
-	flag.Parse()
+// fileLines returns the count of lines in a file.
+func fileLines() (int, error) {
+	content, err := os.ReadFile(lineCount)
 
-	fmt.Println(usage)
-
-	// bytes in the file
-	if fileName != "" {
-		contentLen, err := fileBytes()
-		if err != nil {
-			fmt.Printf("Error while processing file %s", fileName)
-			return
-		}
-		// TODO: Extra "%" symbol in output ex:"335039 fileName %"
-		fmt.Printf("%d %s", contentLen, fileName)
+	if err != nil {
+		return 0, err
 	}
 
+	var lc int
+	for _, c := range content {
+		if c == byte('\n') {
+			lc++
+		}
+	}
+
+	return lc, nil
+}
+
+// fileWords returns the count of words in a file.
+func fileWords() (int, error) {
+	content, err := os.ReadFile(wordCount)
+
+	if err != nil {
+		return 0, err
+	}
+
+	var wc int
+	bc := len(content)
+	for i := 0; i < bc-1; i++ {
+		c := content[i]
+		c1 := content[i+1]
+		if (c == byte(' ') || c == byte('\t') || c == byte('\r') || c == byte('\n')) &&
+			(c1 != byte(' ') && c1 != byte('\t') && c1 != byte('\r') && c1 != byte('\n')) {
+			wc++
+		}
+	}
+	if content[bc-1] != byte(' ') || content[bc-1] == byte('\t') || content[bc-1] == byte('\n') {
+		wc++
+	}
+	return wc, nil
+}
+
+func main() {
+	fmt.Println(usage)
+
+	// TODO: The file is assumed to be in same directory.
+	flag.StringVar(&bytesCount, "c", "", "File name to be parsed for bytes count")
+	flag.StringVar(&lineCount, "l", "", "File name to be parsed for line count")
+	flag.StringVar(&wordCount, "w", "", "File name to be parsed for word count")
+	flag.Parse()
+
+	// If number of bytes are requested.
+	if bytesCount != "" {
+		// bytes in the file
+		contentLen, err := fileBytes()
+		if err != nil {
+			fmt.Printf("Error while processing file %s - %s\n", bytesCount, err)
+			return
+		}
+		fmt.Printf("%d %s\n", contentLen, bytesCount)
+	}
+
+	// If number of lines are requested.
+	if lineCount != "" {
+		// lines in the file
+		contentLen, err := fileLines()
+		if err != nil {
+			fmt.Printf("Error while processing file %s - %s\n", lineCount, err)
+			return
+		}
+		fmt.Printf("%d %s\n", contentLen, lineCount)
+	}
+
+	// If number of words are requested.
+	if wordCount != "" {
+		// words in the file
+		contentLen, err := fileWords()
+		if err != nil {
+			fmt.Printf("Error while processing file %s - %s\n", wordCount, err)
+			return
+		}
+		fmt.Printf("%d %s\n", contentLen, wordCount)
+	}
 }
