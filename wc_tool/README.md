a9a13ba6f62e5e40966abd3770f313d68f9321bc
Piyush <alveusdweller@gmail.com>
2024-03-19 22:58:46 +0530

    [wc_tool] Added -m implementation

--- wc_tool/README.md
+++ wc_tool/README.md
@@ -2,13 +2,56 @@ Problem: https://codingchallenges.fyi/challenges/challenge-wc/
 
 #### Installation
 - go build .
-- ./wc_tool <option> <file_name>
+- ./wc_tool -option file_name
+- Available options:
+    - c: for byte count
+    - l: for line count
+    - w: for word count
+    - m: for character count
 
 
 #### Potential improvements
-- needs to read entire file to memory
+- needs to read entire file to memory, can this be improved?
 
 #### Learnings:
 - Golang
     - flag parsing
     - read file bytes
+    - Rune v/s bytes
+- UTF-8 encoding v/s ASCII encoding
+    - For files encoded in single-byte character sets like ASCII, these two counts will be the same. However, for files encoded in multibyte character sets like UTF-8, where a single character can be represented by multiple bytes, wc -m will give a more accurate count of characters.
+    - character v/s byte
+
+
+#### Results
+```
+╰─⠠⠵ wc -m test.txt
+  339292 test.txt
+╰─⠠⠵ wc -c test.txt
+  342190 test.txt
+╰─⠠⠵ wc -l test.txt
+    7145 test.txt
+╰─⠠⠵ wc -w test.txt
+   58164 test.txt
+╰─⠠⠵ wc test.txt
+    7145   58164  342190 test.txt
+
+
+╰─⠠⠵ ./wc_tool -m test.txt
+339292 test.txt
+╰─⠠⠵ ./wc_tool -c test.txt
+342190 test.txt
+╰─⠠⠵ ./wc_tool -l test.txt
+7145 test.txt
+╰─⠠⠵ ./wc_tool -w test.txt
+58164 test.txt
+╰─⠠⠵ ./wc_tool test.txt
+7145 58164 342190 test.txt
+```
+
+
+#### References:
+- https://blog.ippon.tech/no-framework-approach-to-building-a-cli-with-go/
+- https://pkg.go.dev/flag
+- https://www.scaler.com/topics/golang/golang-read-file/
+- Rune: https://medium.com/@golangda/golang-quick-reference-runes-2f3f117987a6#:~:text=Runes%20in%20Go%20represent%20Unicode,various%20languages%20and%20symbol%20sets.
--- wc_tool/main.go
+++ wc_tool/main.go
@@ -10,11 +10,14 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"unicode/utf8"
 )
 
+var help = flag.Bool("help", false, "Show help")
 var bytesCount string
 var lineCount string
 var wordCount string
+var characterCount string
 
 var usage = `Usage:
 NAME
@@ -22,6 +25,9 @@ NAME
 
 DESCRIPTION
 	-c <filepath>: outputs the number of bytes in a file
+	-l <filepath>: outputs the number of lines in a file
+	-w <filepath>: outputs the number of words in a file
+	-m <filepath>: outputs the number of characters in a file
 `
 
 // fileBytes returns the count of bytes in a file.
@@ -36,7 +42,7 @@ func fileBytes() (int, error) {
 
 // fileLines returns the count of lines in a file.
 func fileLines() (int, error) {
-	content, err := os.ReadFile(lineCount)
+	content, err := os.ReadFile(lineCount) // returns a byte array of content
 
 	if err != nil {
 		return 0, err
@@ -54,7 +60,7 @@ func fileLines() (int, error) {
 
 // fileWords returns the count of words in a file.
 func fileWords() (int, error) {
-	content, err := os.ReadFile(wordCount)
+	content, err := os.ReadFile(wordCount) // returns a byte array of content
 
 	if err != nil {
 		return 0, err
@@ -65,25 +71,70 @@ func fileWords() (int, error) {
 	for i := 0; i < bc-1; i++ {
 		c := content[i]
 		c1 := content[i+1]
-		if (c == byte(' ') || c == byte('\t') || c == byte('\r') || c == byte('\n')) &&
-			(c1 != byte(' ') && c1 != byte('\t') && c1 != byte('\r') && c1 != byte('\n')) {
+		// The next byte has to be not a character
+		if (c != byte(' ') && c != byte('\t') && c != byte('\r') && c != byte('\n')) &&
+			(c1 == byte(' ') || c1 == byte('\t') || c1 == byte('\r') || c1 == byte('\n')) {
 			wc++
 		}
 	}
-	if content[bc-1] != byte(' ') || content[bc-1] == byte('\t') || content[bc-1] == byte('\n') {
+
+	// handling for last character. The last word maynot be followed by a space or new line and wouldn't be
+	// counted in previous loop.
+	c := content[bc-1]
+	if c != byte(' ') && c != byte('\t') && c != byte('\r') && c != byte('\n') {
 		wc++
 	}
+
 	return wc, nil
 }
 
-func main() {
-	fmt.Println(usage)
+// fileCharacters returns the count of characters in a file.
+func fileCharacters() (int, error) {
+	// utf8.RuneCountInString(characterCount) is used to count runes (characters) in a string.
+	content, err := os.ReadFile(characterCount) // returns a byte array of content
 
+	if err != nil {
+		return 0, err
+	}
+
+	runeSlice := make([]rune, 0)
+	for len(content) > 0 {
+		r, size := utf8.DecodeRune(content)
+		runeSlice = append(runeSlice, r)
+		content = content[size:]
+	}
+
+	return len(runeSlice), nil
+}
+
+// isFlagPassed determines if a particular flag was passed to the program.
+func isFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
+}
+
+func main() {
 	// TODO: The file is assumed to be in same directory.
 	flag.StringVar(&bytesCount, "c", "", "File name to be parsed for bytes count")
-	flag.StringVar(&lineCount, "l", "", "File name to be parsed for line count")
+	flag.StringVar(&lineCount, "l", "ddd", "File name to be parsed for line count")
 	flag.StringVar(&wordCount, "w", "", "File name to be parsed for word count")
+	flag.StringVar(&characterCount, "m", "", "File name to be parsed for character count")
 	flag.Parse()
+
+	// if !isFlagPassed("l") {
+	// 	fmt.Println("Yess")
+	// }
+
+	// // Usage demo
+	// if *help {
+	// 	flag.Usage()
+	// 	os.Exit(0)
+	// }
 
 	// If number of bytes are requested.
 	if bytesCount != "" {
@@ -94,6 +145,7 @@ func main() {
 			return
 		}
 		fmt.Printf("%d %s\n", contentLen, bytesCount)
+		return
 	}
 
 	// If number of lines are requested.
@@ -105,6 +157,7 @@ func main() {
 			return
 		}
 		fmt.Printf("%d %s\n", contentLen, lineCount)
+		return
 	}
 
 	// If number of words are requested.
@@ -116,5 +169,46 @@ func main() {
 			return
 		}
 		fmt.Printf("%d %s\n", contentLen, wordCount)
+		return
+	}
+
+	// If number of characters are requested.
+	if characterCount != "" {
+		// characters in the file
+		contentLen, err := fileCharacters()
+		if err != nil {
+			fmt.Printf("Error while processing file %s - %s\n", characterCount, err)
+			return
+		}
+		fmt.Printf("%d %s\n", contentLen, characterCount)
+		return
+	}
+
+	// If no flag was provided, use the second argument as filename
+	args := os.Args[1:]
+	if len(args) > 0 {
+		lastArg := args[len(args)-1]
+		bytesCount = lastArg
+		lineCount = lastArg
+		wordCount = lastArg
+
+		a1, err := fileBytes()
+		if err != nil {
+			fmt.Printf("Error while processing file %s - %s\n", bytesCount, err)
+			return
+		}
+		a2, err := fileLines()
+		if err != nil {
+			fmt.Printf("Error while processing file %s - %s\n", lineCount, err)
+			return
+		}
+		a3, err := fileWords()
+		if err != nil {
+			fmt.Printf("Error while processing file %s - %s\n", wordCount, err)
+			return
+		}
+		fmt.Printf("%d %d %d %s\n", a2, a3, a1, lastArg)
+	} else {
+		fmt.Println(usage)
 	}
 }
